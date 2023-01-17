139ac6bdd7a078ba3e83f1c842cbf83bcdca358e
Nicolas Beusen <nicolas.beusen.ext@proximus.com>
2023-01-17 16:53:39 +0100

    Parse Certificate domains

--- main.go
+++ main.go
@@ -3,8 +3,6 @@ package main
 import (
 	"bufio"
 	"crypto/tls"
-	"encoding/hex"
-	"encoding/json"
 	"fmt"
 	"os"
 	"strings"
@@ -35,33 +33,15 @@ func main() {
 		// Get the certificate from the connection
 		cert := conn.ConnectionState().PeerCertificates[0]
 
-		// Get the serial number of the certificate
-		serial := cert.SerialNumber
-		serialHex := hex.EncodeToString(serial.Bytes())
+		// Extract CommonName
+		commonName := cert.Subject.CommonName
+		fmt.Println(commonName)
 
-		// construct the file name with the serial number
-		fileName := serialHex + ".json"
+		// Extract DNS Names
+		dnsNames := cert.DNSNames
 
-		// convert the certificate to json
-		certJson, err := json.MarshalIndent(cert, "", "  ")
-		if err != nil {
-			fmt.Println("Error marshalling certificate to json: ", err)
-			continue
+		for _, dnsName := range dnsNames {
+			fmt.Printf("%s\n", dnsName)
 		}
-
-		// save the certificate to file
-		certOut, err := os.Create(fileName)
-		if err != nil {
-			fmt.Println("Error creating file: ", err)
-			continue
-		}
-		defer certOut.Close()
-
-		_, err = certOut.Write(certJson)
-		if err != nil {
-			fmt.Println("Error writing json to file: ", err)
-			continue
-		}
-		fmt.Println("Certificate saved to file: ", fileName)
 	}
 }
